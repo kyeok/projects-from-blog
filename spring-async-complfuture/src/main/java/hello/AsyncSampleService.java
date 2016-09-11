a130458ebae5b20b294699d41ee1caa70fb79b7c
harley.kim <harley.kim@daumkakao.com>
2016-09-12 00:40:57 +0900

    Modify scheduler to newSingleThreadScheduledExecutor

--- spring-async-complfuture/src/main/java/hello/AsyncSampleService.java
+++ spring-async-complfuture/src/main/java/hello/AsyncSampleService.java
@@ -1,7 +1,5 @@
 package hello;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +17,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
+ * Reference : http://www.nurkiewicz.com/2014/12/asynchronous-timeouts-with.html
  * Created by Harley on 2016. 9. 11..
  */
 @Service
@@ -65,19 +64,20 @@ public class AsyncSampleService {
                 .thenAccept(this::send);  //probably not what you think
     }
 
-    private static final ScheduledExecutorService scheduler =
-            //1 thread ought to be enough for anybody
-            Executors.newScheduledThreadPool(
-                    1,
-                    new ThreadFactoryBuilder()
-                            .setDaemon(true)
-                            .setNameFormat("failAfter-%d")
-                            .build());
+//    private static final ScheduledExecutorService scheduler =
+//            Executors.newScheduledThreadPool(
+//                    1,
+//                    new ThreadFactoryBuilder()
+//                            .setDaemon(true)
+//                            .setNameFormat("failAfter-%d")
+//                            .build());
 
+    private static final ScheduledExecutorService EXECUTOR
+            = Executors.newSingleThreadScheduledExecutor();
 
     public static <T> CompletableFuture<T> failAfter(Duration duration) {
         final CompletableFuture<T> promise = new CompletableFuture<T>();
-        scheduler.schedule(() -> {
+        EXECUTOR.schedule(() -> {
             final TimeoutException ex = new TimeoutException("Timeout after " + duration);
             return promise.completeExceptionally(ex);
         }, duration.toMillis(), MILLISECONDS);
@@ -100,6 +100,7 @@ public class AsyncSampleService {
     }
 
     public void serve7_within() throws InterruptedException {
+//        final CompletableFuture<User> responseFuture = within(gitHubLookupService.findUser("91ft"), Duration.ofSeconds(1));
         final CompletableFuture<User> responseFuture = within(gitHubLookupService.findUser("91ft"), Duration.ofMillis(1));
         responseFuture
                 .thenAccept(this::send)
